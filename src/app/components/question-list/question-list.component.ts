1306427a5d06f2fbc15f0b317d76060907b7b45d
Suresh <sureshsingam@gmail.com>
2017-09-06 01:34:41 -0300

    Added 2 components and a data service

--- src/app/components/question-list/question-list.component.ts
+++ src/app/components/question-list/question-list.component.ts
@@ -11,11 +11,12 @@ export class QuestionListComponent implements OnInit {
   // Adding local storage to store list of questions
   questions:Question[];
 
+  // when creating a data service, need to inject the data service in the constructor
   constructor(public dataService:DataService) {
     
 
    }
-
+   // when the component loads, ngOnInit runs
   ngOnInit() {
     this.questions = this.dataService.getQuestions();
   }
